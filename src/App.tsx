c70b382ea22cf62b208a6fea31e233309dc769f7
Tal Forsher <talf@webiya.com>
2021-11-26 08:11:13 +0200

    append raw data type and fetch data from server

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+
+interface Raw {
+  name: { common: string };
+  flags: { svg?: string };
+}
 
 interface Country {
   name: string;
-  flag: string;
+  flag?: string;
 }
 
+const fetchCountries = async () => {
+  const data = await fetch("https://restcountries.com/v3.1/all");
+  const fetchedCountriesList: Raw[] = await data.json();
+  const countriesListToUpdate: Country[] = fetchedCountriesList.map((country) => ({
+    name: country.name.common,
+    flag: country.flags?.svg,
+  }));
+};
+
 function App() {
-  const [countriesList, setCountriesList] = useState([]);
+  const [countriesList, setCountriesList] = useState<Country[]>([]);
+  useEffect(() => {
+    fetchCountries();
+  });
   return <div className="App"></div>;
 }
 
