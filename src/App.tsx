66ac05d32ae02053ddc2efc2c304ae8740a8c788
Tal Forsher <talf@webiya.com>
2021-11-26 07:58:51 +0200

    initiate Country type

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
 import "./App.css";
 
+interface Country {
+  name: string;
+  flag: string;
+}
+
 function App() {
   const [countriesList, setCountriesList] = useState([]);
   return <div className="App"></div>;
