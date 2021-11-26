7ff16b3e9a6553db03b71dd3be3455004dd8b70a
Tal Forsher <talf@webiya.com>
2021-11-26 07:56:30 +0200

    initiate state for list if countries

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+  const [countriesList, setCountriesList] = useState([]);
+  return <div className="App"></div>;
 }
 
 export default App;
