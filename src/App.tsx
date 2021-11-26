96601613d42914827f8acef6a1fd002b62eb58a8
Tal Forsher <talf@webiya.com>
2021-11-26 11:51:51 +0200

    refetch method and immutibly update array

--- src/App.tsx
+++ src/App.tsx
@@ -13,37 +13,50 @@ interface Country {
 
 function App() {
   const [countriesList, setCountriesList] = useState<Country[]>([]);
-  const [refetch, setRefetch] = useState<number>(0);
+  const [refetch, setRefetch] = useState<{ query: string; count: number }>({
+    query: "",
+    count: 0,
+  });
   useEffect(() => {
     const fetchCountries = async () => {
-      const data = await fetch("https://restcountries.com/v3.1/all");
+      const data = await fetch(
+        `https://restcountries.com/v3.1/${refetch.count ? `name/${refetch.query}` : "all"}`,
+      );
       const fetchedCountriesList: Raw[] = await data.json();
       const countriesListToUpdate: Country[] = fetchedCountriesList.map((country) => ({
         name: country.name.common,
         flag: country.flags?.svg,
       }));
-      setCountriesList(countriesListToUpdate);
+      setCountriesList(
+        refetch.count
+          ? countriesList.map((country) => {
+              if (country.name === refetch.query) {
+                return Object.assign({}, countriesListToUpdate[0], {
+                  flag: countriesListToUpdate[0].flag,
+                });
+              }
+              return country;
+            })
+          : countriesListToUpdate,
+      );
     };
 
     fetchCountries();
-  }, [refetch]);
+  }, [refetch.count]);
 
   return (
     <div className="App">
       <ul className="countries">
-        {[
-          {
-            name: "test",
-            flag: Math.round(Math.random() * 2) ? "https://flagcdn.com/sd.svg" : undefined,
-          },
-          ...countriesList,
-        ].map((country) => (
-          <li className="country">
-            {country.name}{" "}
-            {country.flag ? (
-              <img width={40} src={country.flag} alt={country.name} />
+        {countriesList.map(({ name, flag }) => (
+          <li key={name} className="country">
+            {name}
+            {flag ? (
+              <img width={40} src={flag} alt={name} />
             ) : (
-              <div className="default" onClick={() => setRefetch((curr) => curr + 1)} />
+              <div
+                className="default"
+                onClick={() => setRefetch((curr) => ({ query: name, count: curr.count + 1 }))}
+              />
             )}
           </li>
         ))}
