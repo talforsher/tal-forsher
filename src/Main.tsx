44267fa578682a59335957c79d764a107420f4d6
Tal Forsher <talf@webiya.com>
2021-11-27 23:23:18 +0200

    refactor App as main and lift useState and useEffect

---
+++ src/Main.tsx
@@ -0,0 +1,38 @@
+import React, { useCallback, useState } from "react";
+import { Link } from "react-router-dom";
+import "./App.css";
+
+const Main = ({ setRefetch, countriesList }: any) => {
+  const [searchQuery, setSearchQuery] = useState<string>("");
+
+  const listToRender = useCallback(
+    () =>
+      countriesList
+        .filter((country: any) => country.name.toLowerCase().includes(searchQuery.toLowerCase()))
+        .map(({ name, flag }: any) => (
+          <li key={name} className="country">
+            <Link to="flag" state={{ image: flag }}>
+              {name}
+            </Link>
+            {flag ? (
+              <img width={40} src={flag} alt={name} />
+            ) : (
+              <div
+                className="default"
+                onClick={() => setRefetch((curr: any) => ({ query: name, count: curr.count + 1 }))}
+              />
+            )}
+          </li>
+        )),
+    [searchQuery, countriesList],
+  );
+
+  return (
+    <div className="App">
+      <input type="text" onChange={(e) => setSearchQuery(e.target.value)} />
+      <ul className="countries">{listToRender()}</ul>
+    </div>
+  );
+};
+
+export default Main;
