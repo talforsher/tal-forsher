ee3c7b3d61e40aac8d1ca7643cfaf7a680c422f1
Tal Forsher <talf@webiya.com>
2022-01-26 11:12:41 +0200

    first test for rendering input field

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,4 +1,50 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, within, fireEvent, waitFor } from "@testing-library/react";
+import App from "./App";
 
+describe("App Container", () => {
+    it("should render input field", async () => {
+        const { container } = render(<App />);
+        await waitFor(
+            () => {
+                expect(
+                    container.querySelector('[class="country"]'),
+                ).toBeInTheDocument();
+            },
+            { timeout: 10000 },
+        );
+
+        expect(container.querySelectorAll('[class="country"]')).toHaveLength(
+            250,
+        );
+
+        let wrapper: any = container.querySelector("input[type='text']");
+        expect(wrapper).toBeDefined();
+        expect(within(wrapper).queryByDisplayValue("")).toBeNull();
+
+        fireEvent.change(wrapper, {
+            target: { value: "Papua New Guinea" },
+        });
+        expect(container.querySelectorAll('[class="country"]')).toHaveLength(1);
+
+        fireEvent.change(wrapper, {
+            target: { value: "" },
+        });
+        expect(container.querySelectorAll('[class="country"]')).toHaveLength(
+            250,
+        );
+
+        fireEvent.change(wrapper, {
+            target: { value: "Papua New Guinea" },
+        });
+        expect(container.querySelectorAll('[class="country"]')).toHaveLength(1);
+        expect(
+            within(wrapper).queryByDisplayValue("Papua New Guinea"),
+        ).toBeDefined();
+
+        const btnWrapper: any = container.querySelector("button");
+        expect(btnWrapper).toBeDefined();
+        fireEvent.click(btnWrapper);
+        expect(within(wrapper).queryByDisplayValue("")).toBeNull();
+    });
+});
--- src/types.tsx
+++ src/types.tsx
@@ -1,14 +1,14 @@
 export interface Raw {
-    name: { common: string };
-    flags: { svg?: string };
-  }
+  name: { common: string };
+  flags: { svg?: string };
+}
 
 export interface Country {
-  name: string;
-  flag?: string;
+name: string;
+flag?: string;
 }
-  
+
 export interface Refetch {
-  query: string;
-  count: number;
+query: string;
+count: number;
 }
